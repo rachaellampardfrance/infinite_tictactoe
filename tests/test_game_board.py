6c7fa284faa18845b32ee4d6d892b9da1439a3c4
rachaellampardfrance <rachaellampardfrance@gmail.com>
2024-10-29 12:17:30 +0000

    Add unit tests for player and bot token assignment
    + Add unit test for GameBoard.player_token()
    + Add unit test for GameBoard.assign_bot_token()
    + update test docstrings and test comments

--- tests/test_game_board.py
+++ tests/test_game_board.py
@@ -10,14 +10,22 @@ def test_init():
             [' ', ' ', ' '],
             [' ', ' ', ' ']
         ]
-    
-    # correct player and bot markers
+    # correct player and bot tokens set
     game_board = GameBoard('X')
     assert game_board.player_token == 'X' and game_board.bot_token == 'O'
     game_board = GameBoard('O')
     assert game_board.player_token == 'O' and game_board.bot_token == 'X'
 
-    # raises errors with invalid player type
+
+def test_player_token():
+    """player token sets token == the argument"""
+    # correct player token assigned 
+    game_board = GameBoard('X')
+    assert game_board.player_token == 'X'
+    game_board = GameBoard('O')
+    assert game_board.player_token == 'O'
+
+    # raises errors: invalid player token
     with pytest.raises(ValueError, match="Not a player"):
         game_board = GameBoard('1')
     with pytest.raises(ValueError, match="Not a player"):
@@ -26,17 +34,34 @@ def test_init():
         game_board = GameBoard('!')
 
 
+def test_assign_bot_token():
+    """should assign opposite token from player"""
+
+    # correct bot token assignment
+    game_board = GameBoard('X')
+    assert game_board.bot_token == 'O'
+    game_board = GameBoard('O')
+    assert game_board.bot_token == 'X'
+
+    # raises error: invalid player token
+    with pytest.raises(ValueError, match="Error assigning 'bot_token', 'self.player_token' not equal to 'X' or 'O'"):
+        game_board = GameBoard('X')
+        game_board.assign_bot_token('a')
+
+
 def test_is_board_location():
     """check function can identify all board locations"""
-    # identifies board locations and non existent locations
+
+    # True: identifies existing board locations
     assert GameBoard.is_board_location('1', '3') == True
     assert GameBoard.is_board_location('3', '1') == True
+    # False: locations are not existing board locations
     assert GameBoard.is_board_location('0', '1') == False
     assert GameBoard.is_board_location('4', '3') == False
     assert GameBoard.is_board_location('1', '0') == False
     assert GameBoard.is_board_location('3', '4') == False
 
-    # raises error if either input value is not a digit
+    # raises error: either input value is not a digit
     with pytest.raises(ValueError, match="One or more inputs is not valid"):
         GameBoard.is_board_location('a', '1')
     with pytest.raises(ValueError, match="One or more inputs is not valid"):
@@ -47,12 +72,15 @@ def test_is_board_location():
         GameBoard.is_board_location('1', '!')
 
 
-def test_valid_placement():
-    # confirms valid placement
+def test_is_valid_placement():
+    """returns True if the location is unoccupied
+    and False if occupied"""
+
+    # True: valid placement
     game_board = GameBoard('X')
     assert game_board.is_valid_placement('1', '1') == True
 
-    # False if location occupied
+    # False: occupied location
     game_board = GameBoard('X')
     game_board.list = [     
         ['X', ' ', ' '],
@@ -61,7 +89,7 @@ def test_valid_placement():
     ]
     assert game_board.is_valid_placement('1', '1') == False
 
-    # raises error if placement is not a board location
+    # raises error: placement is not a board location
     with pytest.raises(ValueError, match="Not a valid location"):
         game_board = GameBoard('X')
         game_board.is_valid_placement('0', '1')
@@ -69,10 +97,11 @@ def test_valid_placement():
         game_board = GameBoard('X')
         game_board.is_valid_placement('1', '4')
 
-    # raises error if passed in placements are not digits
+    # raises error: passed in placements are not digits
     with pytest.raises(ValueError, match="One or more inputs is not valid"):
         game_board = GameBoard('O')
         game_board.is_valid_placement('a', '1')
+
 
 def test_str():
     """test printing the 'game board'
