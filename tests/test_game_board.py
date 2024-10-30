8a470519c04c409fa0be0989b6497276be561b10
rachaellampardfrance <rachaellampardfrance@gmail.com>
2024-10-30 15:32:45 +0000

    Updated GameBoard class unit tests
    + Update str function tests to match new format
    + Add large board str function tests
    + Add generate_lists() tests
    + Add generate_index() tests
    + Add size() tests

--- tests/test_game_board.py
+++ tests/test_game_board.py
@@ -16,6 +16,69 @@ def test_init():
     game_board = GameBoard('O')
     assert game_board.player_token == 'O' and game_board.bot_token == 'X'
 
+    # test setting size value to none default sets
+    #  size, index and list correctly
+    game_board = GameBoard('X', 5)
+    assert game_board.size == 5 and game_board.index == [1, 2, 3, 4, 5] and game_board.list == [     
+            [' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' '],
+            ]
+
+
+def test_size():
+    """size of board is set correctly"""
+
+    # default size
+    game_board = GameBoard('X')
+    assert game_board.size == 3
+    # argument sizes
+    game_board = GameBoard('X', 3)
+    assert game_board.size == 3
+    game_board = GameBoard('X', 4)
+    assert game_board.size == 4
+    game_board = GameBoard('X', 5)
+    assert game_board.size == 5    
+
+    # size only accepts 3-5 range inclusive
+    with pytest.raises(ValueError, match="argument 'size' must be between 3 - 5 inclusive"):
+        game_board = GameBoard('X', 1)
+    with pytest.raises(ValueError, match="argument 'size' must be between 3 - 5 inclusive"):
+        game_board = GameBoard('X', -1)
+    with pytest.raises(ValueError, match="argument 'size' must be between 3 - 5 inclusive"):
+        game_board = GameBoard('X', 6)
+
+    # size only accepts 'int' type
+    with pytest.raises(ValueError, match="'size' must be of type 'int'"):
+        game_board = GameBoard('X', 'a')
+    with pytest.raises(ValueError, match="'size' must be of type 'int'"):
+        game_board = GameBoard('X', '!')
+
+
+def test_generate_index():
+    """index list is generated correctly on init"""
+    game_board = GameBoard('X', 5)
+    assert game_board.generate_index() == [1, 2, 3, 4, 5] 
+    game_board = GameBoard('X', 4)
+    assert game_board.generate_index() == [1, 2, 3, 4] 
+
+
+def test_generate_list():
+    game_board = GameBoard('X')
+    assert game_board.list == [
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']]
+    game_board = GameBoard('X', 5)
+    assert game_board.list == [
+        [' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ']]
+    
 
 def test_player_token():
     """player token sets token == the argument"""
@@ -43,33 +106,28 @@ def test_assign_bot_token():
     game_board = GameBoard('O')
     assert game_board.bot_token == 'X'
 
-    # raises error: invalid player token
-    with pytest.raises(ValueError, match="Error assigning 'bot_token', 'self.player_token' not equal to 'X' or 'O'"):
-        game_board = GameBoard('X')
-        game_board.assign_bot_token('a')
-
 
 def test_is_board_location():
     """check function can identify all board locations"""
 
     # True: identifies existing board locations
-    assert GameBoard.is_board_location('1', '3') == True
-    assert GameBoard.is_board_location('3', '1') == True
+    game_board = GameBoard('X')
+    assert game_board.is_board_location('1', '3') == True
+    assert game_board.is_board_location('3', '1') == True
     # False: locations are not existing board locations
-    assert GameBoard.is_board_location('0', '1') == False
-    assert GameBoard.is_board_location('4', '3') == False
-    assert GameBoard.is_board_location('1', '0') == False
-    assert GameBoard.is_board_location('3', '4') == False
-
+    assert game_board.is_board_location('0', '1') == False
+    assert game_board.is_board_location('4', '3') == False
+    assert game_board.is_board_location('1', '0') == False
+    assert game_board.is_board_location('3', '4') == False
     # raises error: either input value is not a digit
     with pytest.raises(ValueError, match="One or more inputs is not valid"):
-        GameBoard.is_board_location('a', '1')
+        game_board.is_board_location('a', '1')
     with pytest.raises(ValueError, match="One or more inputs is not valid"):
-        GameBoard.is_board_location('!', '1')
+        game_board.is_board_location('!', '1')
     with pytest.raises(ValueError, match="One or more inputs is not valid"):
-        GameBoard.is_board_location('1', 'a')
+        game_board.is_board_location('1', 'a')
     with pytest.raises(ValueError, match="One or more inputs is not valid"):
-        GameBoard.is_board_location('1', '!')
+        game_board.is_board_location('1', '!')
 
 
 def test_is_valid_placement():
@@ -109,17 +167,16 @@ def test_str():
 
     # from initial
     game_board = GameBoard('X')
-    assert game_board.__str__() == """
-        Player: X
-        Computer: O
-         ___________ 
-        |   |   |   |
-        |---|---|---|
-        |   |   |   |
-        |---|---|---|
-        |   |   |   |
-         ‾‾‾‾‾‾‾‾‾‾‾ 
-        """
+    assert game_board.__str__() == (
+        "Player: X\n"
+        "Computer: O\n"
+        " ___________ \n"
+        "|   |   |   |\n"
+        "|---|---|---|\n"
+        "|   |   |   |\n"
+        "|---|---|---|\n"
+        "|   |   |   |\n"
+        " ‾‾‾‾‾‾‾‾‾‾‾ ")
     
     # from ongoing game
     game_board = GameBoard('O')
@@ -128,14 +185,55 @@ def test_str():
         [' ', 'O', 'X'],
         [' ', ' ', ' ']
     ]
-    assert game_board.__str__() == """
-        Player: O
-        Computer: X
-         ___________ 
-        | X |   | O |
-        |---|---|---|
-        |   | O | X |
-        |---|---|---|
-        |   |   |   |
-         ‾‾‾‾‾‾‾‾‾‾‾ 
-        """
+    assert game_board.__str__() == (
+        "Player: O\n"
+        "Computer: X\n"
+        " ___________ \n"
+        "| X |   | O |\n"
+        "|---|---|---|\n"
+        "|   | O | X |\n"
+        "|---|---|---|\n"
+        "|   |   |   |\n"
+        " ‾‾‾‾‾‾‾‾‾‾‾ ")
+    
+
+def test_str_large():
+    """Test boards larger than default size"""
+    # from initial
+    game_board = GameBoard('X', 4)
+    assert game_board.__str__() == (
+        "Player: X\n"
+        "Computer: O\n"
+        " _______________ \n"
+        "|   |   |   |   |\n"
+        "|---|---|---|---|\n"
+        "|   |   |   |   |\n"
+        "|---|---|---|---|\n"
+        "|   |   |   |   |\n"
+        "|---|---|---|---|\n"
+        "|   |   |   |   |\n"
+        " ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾ ")
+    
+    # from ongoing game
+    game_board = GameBoard('X', 5)
+    game_board.list = [     
+        ['X', ' ', 'O', ' ', ' '],
+        [' ', 'O', ' ', ' ', ' '],
+        [' ', ' ', ' ', 'X', ' '],
+        [' ', ' ', 'X', ' ', ' '],
+        [' ', ' ', ' ', ' ', 'O']
+    ]
+    assert game_board.__str__() == (
+        "Player: X\n"
+        "Computer: O\n"
+        " ___________________ \n"
+        "| X |   | O |   |   |\n"
+        "|---|---|---|---|---|\n"
+        "|   | O |   |   |   |\n"
+        "|---|---|---|---|---|\n"
+        "|   |   |   | X |   |\n"
+        "|---|---|---|---|---|\n"
+        "|   |   | X |   |   |\n"
+        "|---|---|---|---|---|\n"
+        "|   |   |   |   | O |\n"
+        " ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾ ")
