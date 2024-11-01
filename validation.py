e00f7e174ae27cbb510414d9b1e811a2a163705b
rachaellampardfrance <rachaellampardfrance@gmail.com>
2024-11-01 13:34:41 +0000

    update validation.py validate digit
    Update validate_digit() to pass *items into validate_str() to resolve error

--- validation.py
+++ validation.py
@@ -4,7 +4,7 @@ def validate_str(*items):
             raise ValueError(f"'{type(item).__name__}' is not of type 'str'")
 
 def validate_digit(*items):
-    validate_str(items)
+    validate_str(*items)
     for item in items:
         if not item.isdigit():
-            raise ValueError(f"{item} is not a digit")
+            raise ValueError(f"'{item}' is not a digit")
