2c4242850bc67528acebadfb6e9d51caf8fcaa1d
rachaellampardfrance <rachaellampardfrance@gmail.com>
2024-11-04 15:32:39 +0000

    Add raise errors to validate functions when no arguments are passed in

--- validation.py
+++ validation.py
@@ -1,4 +1,6 @@
 def validate_str(*items):
+    if not items:
+        raise ValueError(f"expected at least one argument but got none")
     for item in items:
         if not type(item) is str:
             raise ValueError(f"'{type(item).__name__}' is not of type 'str'")
@@ -10,6 +12,8 @@ def validate_digit(*items):
             raise ValueError(f"'{item}' is not a digit")
         
 def validate_int(*items):
+    if not items:
+        raise ValueError(f"expected at least one argument but got none")
     for item in items:
         if not type(item) is int:
             raise ValueError(f"'{type(item).__name__}' is not of type 'int'")
