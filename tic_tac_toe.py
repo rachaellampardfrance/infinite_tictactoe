3c2f97b41f6c3e2608da04054948761ceb00dc95
rachaellampardfrance <rachaellampardfrance@gmail.com>
2024-11-05 08:38:40 +0000

    Add docstrings to functions

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,13 +1,11 @@
 """This program runs a game of tic tac toe"""
+from random import randint
 from game_board import GameBoard
 from tokens import Tokens
-from random import randint
 
 
 def main():
-    # create GameBoard class instance
-    # player_tokens = set_tokens()
-    # game_board = create_game_board()
+    """tic tac toe game creation and game loop"""
     game_board, player_tokens = init_game()
     print_game(game_board, player_tokens)
     
@@ -28,13 +26,14 @@ def main():
 
 
 def init_game():
+    """create game board and set player tokens"""
     board = create_game_board()
     tokens = set_tokens()
     return board, tokens
 
 
 def create_game_board():
-    """create game board class and set player marker"""
+    """create GameBoard object"""
     created = False
     while created == False:
         try:
@@ -45,7 +44,7 @@ def create_game_board():
 
 
 def set_tokens():
-    """Create tokens to be associated with each player"""
+    """Create token object with associated player tokens"""
     created = False
     while created == False:
         try:
@@ -57,33 +56,42 @@ def set_tokens():
 
 
 def print_game(board, tokens):
+    """print player tokens and GameBoard objects"""
     print(tokens)
     print(board)
 
 
-def game_loop(board, player_tokens):
+def game_loop(board, tokens):
+    """loop turns, printing board, 
+    win and stalemate checks, and game on choice
+    """
     game_on = True
     try:
         while game_on:
-                board = user_placement(board, player_tokens)
-                turn(board, player_tokens)
-                board = bot_turn(board, player_tokens)
-                turn(board, player_tokens)
+                board = user_placement(board, tokens)
+                turn(board, tokens)
+                board = bot_turn(board, tokens)
+                turn(board, tokens)
                 game_on = game_on_choice()
     except ValueError as win:
         show_win_message(win)
 
 
-def user_placement(board, player_tokens):
+def user_placement(board, tokens):
+    """get user placement choice and try place"""
     while True:
         placement_choice = get_user_row_column(board)
         try:
-            return try_place(board, placement_choice, player_tokens.user_token)
+            return try_place(board, placement_choice, tokens.user_token)
         except ValueError as e:
             show_error_message(e)
 
 
-def get_user_row_column(board): 
+def get_user_row_column(board):
+    """get user row and column placement 
+    
+    get Gameboard object min max size for user range string
+    and get user input for return dict"""
     board_range = board.min_max()
     min = board_range["min"]
     max = board_range["max"]
@@ -98,9 +106,14 @@ def get_user_row_column(board):
 
 
 def try_place(board, location, token):
-    """board: is the game board object
-    location: is a dict containing "min" and "max" keys
-    token: is the current players token"""
+    """try to place token at location 
+
+    keyword arguments:
+    board -- a GameBoard object
+    location -- a dict containing 
+        {"row": a, "column": b} key variables
+    token -- token 'X' or 'O'
+    """
     row = location["row"]
     column = location["column"]
     board.validate_placement(row, column)
@@ -109,17 +122,22 @@ def try_place(board, location, token):
     return board
 
 
-def bot_turn(board, player_tokens):
-    """basic random placement bot"""
+def bot_turn(board, tokens):
+    """bot placement"""
     while True:
         index_choice = get_easy_bot_choice(board)
         try:
-            return try_place(board, index_choice, player_tokens.bot_token)
+            return try_place(board, index_choice, tokens.bot_token)
         except ValueError as e:
             show_error_message(f"Bot triggered Error: '{e}'")
 
 
 def get_easy_bot_choice(board):
+    """generate random board location choice
+    
+    get the min and max size of the board
+    and generate a random row and column
+    """
     min_max = board.min_max()
     min = int(min_max["min"])
     max = int(min_max["max"])
@@ -130,8 +148,9 @@ def get_easy_bot_choice(board):
 
 
 
-def turn(board, player_tokens):
-    print_game(board, player_tokens)
+def turn(board, tokens):
+    """player turn"""
+    print_game(board, tokens)
     winner = check_for_winner(board)
     if winner:
         raise ValueError(winner)
@@ -140,36 +159,40 @@ def turn(board, player_tokens):
 
 
 def check_for_winner(board):
-    """Check for winner, return winner or None"""
+    """Check for winner
+    
+    return winner or None
+    """
     list = board.list
     winner = ''
 
-    # check if any row is all the same token
+    # check any row is all the same token
     for i in range(3):
-        if list[i][0] == list[i][1] == list[i][2] and not list[i][0] == ' ':
+        if (list[i][0] == list[i][1] == list[i][2] 
+            and not list[i][0] == ' '):
             winner = list[i][0]
 
-    #  check if any column is all the same token
+    #  check any column is all the same token
     for i in range(3):
-        if list[0][i] == list[1][i] == list[2][i] and not list[0][i] == ' ':
+        if (list[0][i] == list[1][i] == list[2][i] 
+            and not list[0][i] == ' '):
             winner = list[0][i]
     
-    #  check is any diagonal is all the same token
-    if list[0][0] == list[1][1] == list[2][2] and not list[0][0] == ' ':
+    #  check any diagonal is all the same token
+    if (list[0][0] == list[1][1] == list[2][2] 
+        and not list[0][0] == ' '):
         winner = list[0][0]
-    elif list[0][2] == list[1][1] == list[2][0] and not list[1][1] == ' ':
+    elif (list[0][2] == list[1][1] == list[2][0] 
+          and not list[1][1] == ' '):
         winner = list[1][1]
 
     if winner:
-        return format_winner_message(winner)
+        return winner
     return None
 
 
-def format_winner_message(winner):
-    return f"WINNER! : {winner}"
-
-
 def is_stalemate(board):
+    """check if stalemate"""
     list = board.list
 
     for i in range(3):
@@ -179,10 +202,8 @@ def is_stalemate(board):
     return True
 
 
-
 def game_on_choice():
-    """Gets user input at the end of each game loop
-    for if the user wants to continue playing"""
+    """check if user wants to continue playing"""
     choice = 'none'
     yes_no = ['Y', 'N']
 
@@ -199,11 +220,13 @@ def game_on_choice():
 
 
 def show_error_message(e):
-    print(e)
+    """format + show error message"""
+    print(f"ERROR: {e}")
 
 
 def show_win_message(win):
-    print(win)
+    """format + show winner message"""
+    print(f"WINNER! : {win}")
 
 
 if __name__ == '__main__':
