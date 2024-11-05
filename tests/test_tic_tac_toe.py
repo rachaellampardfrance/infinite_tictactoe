51794a535345679f3a9fe651440763dd2ec83f81
rachaellampardfrance <rachaellampardfrance@gmail.com>
2024-11-05 10:00:52 +0000

    Update test_tic_tac_toe.py assertions to match new function output

--- tests/test_tic_tac_toe.py
+++ tests/test_tic_tac_toe.py
@@ -12,21 +12,21 @@ def test_check_for_winner():
         [' ', 'X', ' '],
         ['O', ' ', 'X']
     ]
-    assert check_for_winner(game_board) == "WINNER! : X"
+    assert check_for_winner(game_board) == "X"
     # left to right:  O win
     game_board.list = [
         ['O', 'X', 'X'],
         ['X', 'O', ' '],
         [' ', ' ', 'O']
     ]
-    assert check_for_winner(game_board) == "WINNER! : O"
+    assert check_for_winner(game_board) == "O"
     # right to left: X win
     game_board.list = [
         [' ', 'O', 'X'],
         [' ', 'X', 'O'],
         ['X', ' ', 'O']
     ]
-    assert check_for_winner(game_board) == "WINNER! : X"
+    assert check_for_winner(game_board) == "X"
 
     # row wins
     # X win
@@ -35,14 +35,14 @@ def test_check_for_winner():
         [' ', ' ', 'O'],
         ['O', ' ', 'O']
     ]
-    assert check_for_winner(game_board) == "WINNER! : X"
+    assert check_for_winner(game_board) == "X"
     # O win
     game_board.list = [
         ['X', ' ', 'X'],
         [' ', 'X', 'O'],
         ['O', 'O', 'O']
     ]
-    assert check_for_winner(game_board) == "WINNER! : O"
+    assert check_for_winner(game_board) == "O"
 
     # colum wins
     # X win
@@ -51,11 +51,11 @@ def test_check_for_winner():
         [' ', 'O', 'X'],
         ['O', 'O', 'X']
     ]
-    assert check_for_winner(game_board) == "WINNER! : X"
+    assert check_for_winner(game_board) == "X"
     # O win
     game_board.list = [
         ['X', 'O', ' '],
         [' ', 'O', 'X'],
         ['O', 'O', 'X']
     ]
-    assert check_for_winner(game_board) == "WINNER! : O"
+    assert check_for_winner(game_board) == "O"
